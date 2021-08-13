a50d98e007d1c6d8828b675038d751b23bbad754
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-13 18:52:00 -0400

    fixed post using readFileSync and added new package uniqui for ids

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 // fs is a Node standard library package for reading and writing files
 const fs = require("fs");
+//https://www.npmjs.com/package/uniquid
+const uniquid = require("uniquid");
 //setup the Express app:
 const app = express();
 
@@ -16,28 +18,29 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //DATA ========================
-const notes = [
-  {
-    title: "Express Server",
-    text: "1st step, get my Express Server setup. require('expres') is needed.",
-  },
-  {
-    title: "HTML Routes",
-    text: "Add html routes. There are 2, index.html and notes.html",
-  },
-  {
-    title: "API Routes",
-    text: "Add API routes. There will be 2 needed: GET /api/notes & POST /api/notes",
-  },
-  {
-    title: "__dirname Dependency",
-    text: "require('path') to use on path.join(__dirname..",
-  },
-  {
-    title: "Express Data Parsing.",
-    text: "Make sure to do app.use(express.. for both urlencoded & json.",
-  },
-];
+const notes = [];
+// const notes = [
+//   {
+//     title: "Express Server",
+//     text: "1st step, get my Express Server setup. require('expres') is needed.",
+//   },
+//   {
+//     title: "HTML Routes",
+//     text: "Add html routes. There are 2, index.html and notes.html",
+//   },
+//   {
+//     title: "API Routes",
+//     text: "Add API routes. There will be 2 needed: GET /api/notes & POST /api/notes",
+//   },
+//   {
+//     title: "__dirname Dependency",
+//     text: "require('path') to use on path.join(__dirname..",
+//   },
+//   {
+//     title: "Express Data Parsing.",
+//     text: "Make sure to do app.use(express.. for both urlencoded & json.",
+//   },
+// ];
 
 //3) HANDLE REQUEST =============================
 //with Express, this is the callback function inside the .get
@@ -53,12 +56,11 @@ app.get("/notes", (req, res) =>
 
 //API ROUTES ======================
 //-- /api/notes GET
-//app.get("/api/notes", (req, res) => res.json(notes));
-//instead of temp array with note, use readFileSync?
 app.get("/api/notes", (req, res) => {
   let rawData = fs.readFileSync("./db/db.json", "utf8");
   let notesData = JSON.parse(rawData);
-  //console.log(notesData); //this worked!!
+  console.log("app.get /api/notes -----");
+  console.log(notesData);
   res.json(notesData);
 });
 
@@ -66,12 +68,43 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
   // req.body is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
+  /* Instead of this using code array, trying with writeFileSync
   const newNote = req.body;
 
   console.log(newNote.title, newNote.text);
 
   notes.push(newNote);
   res.json(newNote);
+  */
+  //must read file 1st and parse out its data in order to add new data to it:
+  let rawData = fs.readFileSync("./db/db.json", "utf8");
+  let notesData = JSON.parse(rawData);
+  console.log("app.post /api/notes -----");
+  console.log("notesData, parsed from file -----");
+  console.log(notesData);
+  //this gets the new data
+  const newNoteData = req.body;
+  console.log("newNoteData -----");
+  console.log(newNoteData);
+
+  //uniquid() usage/example:
+  //uniquid(); // atquj2qdjig4
+  //this uses uniquid package to generate a unique id assigning it as a property named id to the new data
+  console.log("uniquid()");
+  console.log(uniquid());
+  newNoteData.id = uniquid();
+  console.log("newNoteData[0].id unique id:");
+  console.log(newNoteData.id);
+
+  //this adds the new data to what was in file
+  notesData.push(newNoteData);
+  let newNotesData = JSON.stringify(notesData);
+  fs.writeFileSync("./db/db.json", newNotesData);
+
+  console.log("app.post /api/notes -----");
+  console.log(newNotesData);
+  //refreshes response with all the data that now includes new note
+  res.json(newNotesData);
 });
 
 //4) CREATE SERVER ==============================
