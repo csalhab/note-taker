2b61ed676b53212878b4fba6e37d8d7cfdcf3f48
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-12 21:45:20 -0400

    fixed notes.html route so it comes up

--- server.js
+++ server.js
@@ -14,15 +14,40 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //DATA ========================
+const notes = [
+  {
+    title: "Express Server",
+    text: "1st step, get my Express Server setup. require('expres') is needed.",
+  },
+  {
+    title: "HTML Routes",
+    text: "Add html routes. There are 2, index.html and notes.html",
+  },
+  {
+    title: "API Routes",
+    text: "Add API routes. There will be 2 needed: GET /api/notes & POST /api/notes",
+  },
+  {
+    title: "__dirname Dependency",
+    text: "require('path') to use on path.join(__dirname..",
+  },
+  {
+    title: "Express Data Parsing.",
+    text: "Make sure to do app.use(express.. for both urlencoded & json.",
+  },
+];
 
 //HTML ROUTES ======================
 //-- home page
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 //-- notes page
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "notes.html")));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
 
 //API ROUTES ======================
+app.get("/api/notes", (req, res) => res.json(notes));
 
 //3) HANDLE REQUEST =============================
 
