cbe28231cbe443da7dd66e15ed6edabe7dd04df7
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-13 19:34:47 -0400

    removed console logs and retested confirmed

--- server.js
+++ server.js
@@ -37,8 +37,6 @@ app.get("/notes", (req, res) =>
 app.get("/api/notes", (req, res) => {
   let rawData = fs.readFileSync("./db/db.json", "utf8");
   let notesData = JSON.parse(rawData);
-  console.log("app.get /api/notes -----");
-  console.log(notesData);
   res.json(notesData);
 });
 
@@ -50,22 +48,13 @@ app.post("/api/notes", (req, res) => {
   //must read file 1st and parse out its data in order to add new data to it:
   let rawData = fs.readFileSync("./db/db.json", "utf8");
   let notesData = JSON.parse(rawData);
-  console.log("app.post /api/notes -----");
-  console.log("notesData, parsed from file -----");
-  console.log(notesData);
   //this gets the new data
   const newNoteData = req.body;
-  console.log("newNoteData -----");
-  console.log(newNoteData);
 
   //uniquid() usage/example:
   //uniquid(); // atquj2qdjig4
   //this uses uniquid package to generate a unique id assigning it as a property named id to the new data
-  console.log("uniquid()");
-  console.log(uniquid());
   newNoteData.id = uniquid();
-  console.log("newNoteData[0].id unique id:");
-  console.log(newNoteData.id);
 
   //this adds the new data to what was in file
   notesData.push(newNoteData);
@@ -73,8 +62,6 @@ app.post("/api/notes", (req, res) => {
   let newNotesData = JSON.stringify(notesData);
   fs.writeFileSync("./db/db.json", newNotesData);
 
-  console.log("app.post /api/notes -----");
-  console.log(newNotesData);
   //response available with all the data that now includes new note
   res.json(newNotesData);
 });
@@ -82,26 +69,20 @@ app.post("/api/notes", (req, res) => {
 //-- /api/notes DELETE - bonus
 app.delete("/api/notes/:id", (req, res) => {
   /*
-  -should receive a query parameter containing the id of a note to delete. 
-  -In order to delete a note, you'll need to read all notes from the db.json. file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+  should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json. file, remove the note with the given id property, and then rewrite the notes to the db.json file.
   */
-  console.log(req.params.id);
   //-should receive a query parameter containing the id of a note to delete.
   const noteIdToDelete = req.params.id;
   //-In order to delete a note, you'll need to read all notes from the db.json. file
   let rawData = fs.readFileSync("./db/db.json", "utf8");
   let notesData = JSON.parse(rawData);
   //-remove the note with the given id property
-  console.log(notesData);
   notesData.splice(noteIdToDelete, 1);
-  console.log(notesData);
   //-and then rewrite the notes to the db.json file
   //make data valid JSON & write it to file
   let newNotesData = JSON.stringify(notesData);
   fs.writeFileSync("./db/db.json", newNotesData);
 
-  console.log("app.delete /api/notes -----");
-  console.log(newNotesData);
   //response available with all the data that now includes new note
   res.json(newNotesData);
 });
